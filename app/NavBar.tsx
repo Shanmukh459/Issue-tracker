5445384bd0fabfa52412d57ff567daddf048a0ec
Shanmukh459 <saikrrish459@gmail.com>
2024-10-11 16:51:40 -0700

    remove overriding skeletons by flattening the structure

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
 
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
+    { label: "Issues", href: "/issues/list" },
   ]
   return (
     <nav className="flex space-x-6 mb-5 border-b px-4 h-14 items-center">
