99eeba464588604d18c4e47381939410727af110
Shanmukh459 <saikrrish459@gmail.com>
2024-10-18 17:02:55 -0700

    secure the application by adding middleware

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,12 +1,16 @@
+import { authOptions } from "@/app/auth/authOptions"
 import { issueSchema } from "@/app/validationSchemas"
 import prisma from "@/prisma/client"
+import { getServerSession } from "next-auth"
 import { NextRequest, NextResponse } from "next/server"
-import delay from "delay"
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions)
+  if (!session) return NextResponse.json({}, { status: 401 })
+
   const body = await request.json()
   const validation = issueSchema.safeParse(body)
 
@@ -37,7 +41,8 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  await delay(2000)
+  const session = await getServerSession(authOptions)
+  if (!session) return NextResponse.json({}, { status: 401 })
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   })
